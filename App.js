b08d481e205f8b657c7a949c9784f0efd6cbe461
Romain Tello <romain.tello@gmail.com>
2020-01-24 09:45:34 +0100

    Add first Axios request

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Axios from 'axios';
+
+const APICall = () => {
+  Axios.get('https://randomuser.me/api/')
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error))
+  ;
+}
 
 export default function App() {
+  APICall();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
